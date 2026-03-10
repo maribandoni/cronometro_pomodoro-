94e312bcba043489939d95fcb99128dba22f55bc
unknown <dev@DSN1191348075.edu119.local>
2026-03-10 11:15:14 -0300

    10/03'

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './styles/global.css';
+import './styles/theme.css'
+import { Heading } from './components/Heading';
+import { Container } from './components/Container';
+
+export function App() {
+  
+  return (
+    <>
+      <Container>
+        <Heading>Logo</Heading>
+      </Container>
+      <Container>
+        <Heading>Menu</Heading>
+      </Container>
+    </>
+  )
+}
+
+
---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.css'
+import {TimerIcon } from 'lucide-react'
+
+
+export function Logo(){
+return(
+    <div>
+<a href = "#"> <TimerIcon/><span>Teste</span></a>
+    </div>
+)
+}
